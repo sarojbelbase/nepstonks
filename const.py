a526af91bf1f02fa7c67f0c32765030d2b327932
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-02 13:15:21 +0545

    🎉 start ignition

---
+++ const.py
@@ -0,0 +1,11 @@
+from os import environ
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+
+current_dir = Path(__file__).parent.resolve()
+
+load_dotenv(find_dotenv())
+CHANNEL = environ['CHANNEL']
+BOT_TOKEN = environ['BOT_TOKEN']
+BOT_USERNAME = environ['BOT_USERNAME']
+DATABASE_URI = current_dir / 'stocks.db'
