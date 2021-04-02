a129767a48a9534d294c2a153770491cbbd6c612
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-02 20:27:24 +0545

    🔒 add url to env

--- const.py
+++ const.py
@@ -5,6 +5,7 @@ from dotenv import find_dotenv, load_dotenv
 current_dir = Path(__file__).parent.resolve()
 
 load_dotenv(find_dotenv())
+API_URL = environ['API_URL']
 CHANNEL = environ['CHANNEL']
 BOT_TOKEN = environ['BOT_TOKEN']
 BOT_USERNAME = environ['BOT_USERNAME']
