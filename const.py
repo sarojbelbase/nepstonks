98cb01aa55ab04a6bcb3ca15f654d705b80bbdbc
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-02 22:55:55 +0545

    🔒 env: add origin and referer

--- const.py
+++ const.py
@@ -5,7 +5,9 @@ from dotenv import find_dotenv, load_dotenv
 current_dir = Path(__file__).parent.resolve()
 
 load_dotenv(find_dotenv())
+ORIGIN = environ['ORIGIN']
 API_URL = environ['API_URL']
+REFERER = environ['REFERER']
 CHANNEL = environ['CHANNEL']
 BOT_TOKEN = environ['BOT_TOKEN']
 BOT_USERNAME = environ['BOT_USERNAME']
