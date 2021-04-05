0149341e37e85d4cbfe6f06d2f2e03ef1cfc24d9
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-05 17:07:24 +0545

    ♻️ add docstring and move fns

--- telegram.py
+++ telegram.py
@@ -7,24 +7,6 @@ from const import CHANNEL, HORI_LINE, PDF_URL, TELEGRAM_URL
 from insert import mark_as_published
 
 
-def handle_response(the_url, payload):
-    try:
-        response = requests.post(the_url, data=payload)
-        response.raise_for_status()
-        return print(response.content.decode())
-    except requests.exceptions.HTTPError as error:
-        return print(error.response.text)
-
-
-def publish_stock(the_stock):
-    if pdf_url_resolves(the_stock.pdf):
-        send_with_pdf(the_stock)
-        mark_as_published(the_stock)
-    else:
-        send_only_content(the_stock)
-        mark_as_published(the_stock)
-
-
 def send_only_content(stock_detail: str):
     endpoint = TELEGRAM_URL + 'sendMessage'
     payload = {
@@ -74,7 +56,7 @@ def parse_date(given_date: date) -> str:
 
 
 def is_rightshare(stock: str) -> str:
-    # if its right share, publish both units and ratios else units only
+    # if its right share, publish both units & ratio else publish units only
     units = f"Units: <strong>{stock.units}</strong>"
     ratio = f"Ratio: <strong>{stock.ratio}</strong>"
     if stock.ratio:
@@ -84,9 +66,35 @@ def is_rightshare(stock: str) -> str:
 
 
 def pdf_url_resolves(pdf: str) -> bool:
+    """Tells if pdf link still working if the stock has pdf
+
+    Args:
+        pdf (str): pdf filename with pdf extension
+
+    Returns:
+        bool: returns False if it can't resolve the pdf link else returns True
+    """
     if pdf:
         pdf_url = PDF_URL + pdf
         request = requests.get(pdf_url)
         return True if request.status_code == 200 else False
     else:
         return False
+
+
+def handle_response(the_url, payload):
+    try:
+        response = requests.post(the_url, data=payload)
+        response.raise_for_status()
+        return print(response.content.decode())
+    except requests.exceptions.HTTPError as error:
+        return print(error.response.text)
+
+
+def publish_stock(the_stock):
+    if pdf_url_resolves(the_stock.pdf):
+        send_with_pdf(the_stock)
+        mark_as_published(the_stock)
+    else:
+        send_only_content(the_stock)
+        mark_as_published(the_stock)
