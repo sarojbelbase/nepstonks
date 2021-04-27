0ceff1727dd42d5ca981dfb137ae12bd068effae
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-27 18:55:06 +0545

    🥅 remove various error handlers

--- insert.py
+++ insert.py
@@ -10,63 +10,61 @@ TelegramTable = Query(Telegram, session)
 
 
 def add_stock():
-
-    try:
-        scraped_stocks: list = latest_stocks()
-        fetched_stocks: int = len(scraped_stocks)
-    except Exception as error:
-        print("Sorry couldn't connect to the API.", error)
-
-    for the_stock in scraped_stocks:
-        the_stock_id = StockTable.filter(
-            Stock.investment_id == the_stock['investment_id']).first()
-
-        # to avoid inserting the same stock in the table
-        if not the_stock_id and fetched_stocks > 0:
-            this_stock = \
-                Stock(
-                    company_name=the_stock['company_name'],
-                    end_date=the_stock['end_date'],
-                    investment_id=the_stock['investment_id'],
-                    issued_by=the_stock['issued_by'],
-                    pdf=the_stock['pdf'],
-                    ratio=the_stock['ratio'],
-                    start_date=the_stock['start_date'],
-                    stock_id=the_stock['stock_id'],
-                    stock_symbol=the_stock['stock_symbol'],
-                    stock_type=the_stock['stock_type'],
-                    units=the_stock['units']
-                )
-            session.add(this_stock)
-    session.commit()
+    scraped_stocks: list = latest_stocks()
+    fetched_stocks: int = len(scraped_stocks)
+    if fetched_stocks > 0:
+        for the_stock in scraped_stocks:
+            the_stock_id = StockTable.filter(
+                Stock.investment_id == the_stock['investment_id']).first()
+            # to avoid inserting the same stock in the table
+            if not the_stock_id:
+                this_stock = \
+                    Stock(
+                        company_name=the_stock['company_name'],
+                        end_date=the_stock['end_date'],
+                        investment_id=the_stock['investment_id'],
+                        issued_by=the_stock['issued_by'],
+                        pdf=the_stock['pdf'],
+                        ratio=the_stock['ratio'],
+                        start_date=the_stock['start_date'],
+                        stock_id=the_stock['stock_id'],
+                        stock_symbol=the_stock['stock_symbol'],
+                        stock_type=the_stock['stock_type'],
+                        units=the_stock['units']
+                    )
+                session.add(this_stock)
+        session.commit()
 
 
 def add_article():
+    scraped_articles: list = latest_articles()
+    fetched_articles: int = len(scraped_articles)
+    if fetched_articles > 0:
+        for the_article in scraped_articles:
+            the_article_id = NewsTable.filter(
+                News.title == the_article['title']).first()
+            # to avoid inserting the same article in the table
+            if not the_article_id:
+                this_article = \
+                    News(
+                        date_published=the_article['date_published'],
+                        description=the_article['description'],
+                        image_url=the_article['image_url'],
+                        lang=the_article['lang'],
+                        source=the_article['source'],
+                        title=the_article['title'],
+                        url=the_article['url'],
+                    )
+                session.add(this_article)
+        session.commit()
 
-    try:
-        scraped_articles: list = latest_articles()
-        fetched_articles: int = len(scraped_articles)
-    except Exception as error:
-        print("Sorry couldn't connect to the API.", error)
 
-    for the_article in scraped_articles:
-        the_article_id = NewsTable.filter(
-            News.title == the_article['title']).first()
-
-        # to avoid inserting the same article in the table
-        if not the_article_id and fetched_articles > 0:
-            this_article = \
-                News(
-                    date_published=the_article['date_published'],
-                    description=the_article['description'],
-                    image_url=the_article['image_url'],
-                    lang=the_article['lang'],
-                    source=the_article['source'],
-                    title=the_article['title'],
-                    url=the_article['url'],
-                )
-            session.add(this_article)
-    session.commit()
+def add_chat(stock_id: int, message_id: int):
+    the_stock = StockTable.filter(Stock.id == stock_id).first()
+    if the_stock and message_id:
+        chat = Telegram(stock=the_stock, message_id=message_id)
+        session.add(chat)
+        session.commit()
 
 
 def unsent_stocks():
@@ -82,11 +80,3 @@ def upcoming_stocks():
 
 def unsent_articles():
     return NewsTable.filter(News.is_published == False).order_by(News.date_published.desc()).all()
-
-
-def insert_message_id(stock_id: int, message_id: int) -> None:
-    the_stock = StockTable.filter(Stock.id == stock_id).first()
-    if the_stock and message_id:
-        chat = Telegram(stock=the_stock, message_id=message_id)
-        session.add(chat)
-        session.commit()
