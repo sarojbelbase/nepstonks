d9d16039d295532200fe7bc15834627f03d704b9
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-05-01 17:11:49 +0545

    🎨 add more verbosity

--- stocks.py
+++ stocks.py
@@ -1,5 +1,5 @@
 from insert import add_stock, unsent_stocks, upcoming_stocks
-from telegram import publish_stock, remind_and_pin
+from telegram import remind_and_pin
 
 
 def add_and_publish_stocks():
@@ -8,14 +8,11 @@ def add_and_publish_stocks():
 
     unpublished_stocks = list(unsent_stocks())
     if len(unpublished_stocks) > 0:
-        # stocks that are older has to be published first
-        # making the latest ones to be after the older ones
-        the_list = unpublished_stocks[::-1]
-        for the_stock in the_list:
-            publish_stock(the_stock)
+        from image import generate
         from models import session
+        generate(unpublished_stocks)
         session.commit()
-        return {"ok": "true"}
+        return print(f"published {len(unpublished_stocks)} stocks")
 
 
 def remind_about_the_stock():
@@ -23,9 +20,9 @@ def remind_about_the_stock():
     if len(upcoming_issues) > 0:
         for issue in upcoming_issues:
             remind_and_pin(issue)
-        return {"ok": "true"}
+        return print(f"reminded about {len(upcoming_issues)} stock")
 
 
 if __name__ == '__main__':
     add_and_publish_stocks()
-    remind_about_the_stock()
+    # remind_about_the_stock()
