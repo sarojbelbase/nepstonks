785578f5646503d8274a82ce9752cb59fcae9142
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-05-01 21:51:08 +0545

    🚧 rename functions

--- stocks.py
+++ stocks.py
@@ -1,22 +1,21 @@
 from insert import add_stock, unsent_stocks, upcoming_stocks
-from telegram import remind_and_pin
+from telegram import publish_stock, remind_and_pin
 
 
-def add_and_publish_stocks():
+def publish_stocks():
     # fetch and store new stocks before publishing to the channel
     add_stock()
 
     unpublished_stocks = list(unsent_stocks())
     if len(unpublished_stocks) > 0:
-        from image import generate
         from models import session
         for the_stock in unpublished_stocks:
-            generate(the_stock)
+            publish_stock(the_stock)
         session.commit()
         return print(f"published {len(unpublished_stocks)} stocks")
 
 
-def remind_about_the_stock():
+def remind_stock():
     upcoming_issues = list(upcoming_stocks())
     if len(upcoming_issues) > 0:
         for issue in upcoming_issues:
@@ -25,5 +24,5 @@ def remind_about_the_stock():
 
 
 if __name__ == '__main__':
-    add_and_publish_stocks()
-    # remind_about_the_stock()
+    publish_stocks()
+    remind_stock()
